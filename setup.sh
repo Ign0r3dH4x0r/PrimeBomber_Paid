89a8d829e0dcdab993979840df233c266b91500e
BLOODY SOWMIK <69670910+Ign0r3dH4x0r@users.noreply.github.com>
2021-12-14 21:40:11 +0600

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,8 @@
+echo "Installing..."
+pip install requests
+pip install halo 
+pip install termcolor
+pip install reprint 
+echo "All packages might be installed. If you get error, then contact admin."
+sleep 2
+xdg-open https://facebook.com/ffsowmik
